aa2e78bc71620ff0a1246599b4eeeba349930b98
rambip <apero1808@gmail.com>
2023-11-08 15:55:40 +0100

    changed build pipeline

--- flake.nix
+++ flake.nix
@@ -1,46 +1,109 @@
 {
-    description = "yew library to convert markdown to html";
+    description = "a yew markdown component";
+
     inputs = {
-        wasm-tooling.url = github:rambip/wasm-tooling;
-        flake-utils.url = github:numtide/flake-utils;
-        rust-overlay.url = github:oxalica/rust-overlay;
+        flake-utils.url = "github:numtide/flake-utils";
+        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+        rust-overlay = {
+            url = "github:oxalica/rust-overlay";
+            inputs = {
+                flake-utils.follows = "flake-utils";
+            };
+        };
+        crane = {
+          url = "github:ipetkov/crane";
+          inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
 
-    outputs = {self, nixpkgs, flake-utils, rust-overlay, wasm-tooling}: 
-        with flake-utils.lib ;
-        eachSystem ["x86_64-linux" "x86_64-darwin"] (system:
-            let overlays = [rust-overlay.overlays.default];
-                pkgs = import nixpkgs {inherit system overlays;};
-                rust-tooling = pkgs.callPackage wasm-tooling.lib."${system}".rust {
-                    cargo-toml = ./Cargo.toml;
-                    rust-toolchain = pkgs.rust-bin.stable."1.69.0".minimal; 
-                };
-                build = example_name: rust-tooling.buildWithTrunk {
-                 src=./.;
-                 fixRelativeUrl = true;
-                 relativeHtmlTarget = "examples/${example_name}/index.html";
+    outputs = { self, rust-overlay, nixpkgs, flake-utils, crane }: 
+        flake-utils.lib.eachDefaultSystem (system:
+        let 
+            pkgs = import nixpkgs {
+                inherit system;
+                overlays = [ (import rust-overlay) ];
             };
-                examples = builtins.readDir ./examples;
-                built_examples = builtins.mapAttrs (name: value: build name) examples;
-                generate_copy_command = name : ''cp -r ${build name} $out/${name}'';
-                copy_commands = builtins.map generate_copy_command (
-                   builtins.attrNames (builtins.readDir ./examples)
-                );
+            inherit (pkgs) lib;
+
+            rustToolchain = pkgs.rust-bin.selectLatestNightlyWith(
+                toolchain: toolchain.default.override 
+                {
+                    # Set the build targets supported by the toolchain,
+                    # wasm32-unknown-unknown is required for trunk.
+                    targets = [ "wasm32-unknown-unknown" ];
+                }
+            );
+            craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+
+            # discard the directories inside examples to build the library less often
+            libSrc = lib.cleanSourceWith {
+                src = ./.; 
+                filter = path: type:
+                    (builtins.match ".*examples/.*" path == null)
+                    && (craneLib.filterCargoSources path type)
+                    ;
+            };
+
+            fullSrc = lib.cleanSourceWith {
+                src = ./.;
+                filter = path: type:
+                    (lib.hasSuffix "\.html" path) || (lib.hasSuffix "\.css" path)
+                    || (craneLib.filterCargoSources path type)
+                    ;
+            };
+
+            CARGO_BUILD_TARGET = "wasm32-unknown-unknown";
+
+            # Build *just* the cargo dependencies, so we can reuse
+            # all of that work (e.g. via cachix) when running in CI
+            cargoArtifacts = craneLib.buildDepsOnly {
+                inherit CARGO_BUILD_TARGET;
+                src = libSrc;
+                doCheck = false;
+            };
+
+            buildExample = name: craneLib.buildTrunkPackage {
+                inherit CARGO_BUILD_TARGET cargoArtifacts;
+                src = fullSrc;
+                pname = "leptos-markdown-${name}";
+                trunkIndexPath = "examples/${name}/index.html";
+                cargoExtraArgs = "--package=./examples/${name}";
+                # RELATIVE URLS are a MESS 
+                # https://github.com/thedodd/trunk/pull/470
+                trunkExtraBuildArgs = "--public-url=/leptos-markdown/${name}";
+
+                nativeBuildInputs = [
+                    (pkgs.wasm-bindgen-cli.override {
+                        version = "0.2.87";
+                        hash = "sha256-0u9bl+FkXEK2b54n7/l9JOCtKo+pb42GF9E1EnAUQa0=";
+                        cargoHash = "sha256-AsZBtE2qHJqQtuCt/wCAgOoxYMfvDh8IzBPAOkYSYko=";
+                    })
+                ];
+            };
+            example_names = builtins.attrNames(builtins.readDir ./examples);
+            attr_examples = builtins.map 
+                (name: {inherit name; path=buildExample name; value=buildExample name;}) 
+                example_names;
+
+            examples = builtins.listToAttrs attr_examples;
             in
             {
-                packages = built_examples // {
-                    default = 
-                        nixpkgs.legacyPackages."${system}".stdenv.mkDerivation {
-                         name = "markdown examples";
-                         src = "/dev/null";
-                         phases = [ "installPhase" ];
-                         installPhase = ''
-                         mkdir $out
-                         ${builtins.concatStringsSep "\n" copy_commands}
-                         '';
-                     };
+                checks = {};
+                packages = examples // {
+                    default = pkgs.linkFarm "leptos-markdown examples" attr_examples;
                 };
-                devShells.default = rust-tooling.devShell;
+
+                devShells.default = pkgs.mkShell {
+                    buildInputs = with pkgs; [
+                        rustToolchain
+                        binaryen
+                        openssl 
+                        pkg-config
+                        trunk
+                        rust-analyzer
+                    ];
+                };
             }
-        );
+    );
 }
+
